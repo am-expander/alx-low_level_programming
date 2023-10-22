09e33e6531c75067e6b4d67aba1fd778884af88e
am-expander <abdulbasitissah57@gmail.com>
2023-10-21 18:13:48 -0700

    Recursion (Low Level Programming)

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns factorial of a number
+ * @n: number to return the factorial from
+ *
+ * Return: Factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	return (-1);
+	if (n == 0)
+	return (1);
+	return (n * factorial(n - 1));
+}
